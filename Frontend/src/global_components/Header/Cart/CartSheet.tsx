15cc133c73243e2b051e7fa3ccda6ee83f4f9109
TheCrazyDucks <thecrazyducksgopro@gmail.com>
2024-06-08 22:04:15 +0300

    Cart Sheet Changes

--- Frontend/src/global_components/Header/Cart/CartSheet.tsx
+++ Frontend/src/global_components/Header/Cart/CartSheet.tsx
@@ -1,22 +1,48 @@
-import { useState } from "preact/hooks"
+import { useEffect, useState } from "preact/hooks"
 import { Product } from "../../../Types/globalTypes"
-import { ProductCounter } from "../../../pages/ProductPreviewPage/ProductPreview"
+import { Arrow, ProductCounter } from "../../../pages/ProductPreviewPage/ProductPreview"
+import { Link } from "react-router-dom"
+
+const amountHook = ()=>{
+}
 
 const CartSheet: React.FC = () => {
-    const [products, setProducts] = useState<Array<Product> | undefined>()
+    const [products, setProducts] = useState<Array<Product> | undefined>([{
+        _id: "",
+        category: "",
+        img: "",
+        name: "wow",
+        price: 40
+    }])
+    const [amounts, setAmounts] = useState<Array<number> | undefined>()
+
+    useEffect(()=>{
+        setAmounts(products?.map(() => 2))
+    }, [products])
 
     return (
         <div className={"cart-sheet"}>
+            <div className={"cart-sheet-close"}>
+                <Arrow/>
+            </div>
             <div className={"cart-sheet-title"}>
                 מוצרים{ },
                 סה״כ פריטים:
                 { }
             </div>
-            <div className={"cart-sheet-products"}>
-                <ProductItem amount={1} />
+            <div className={"cart-sheet-products scrollbar"}>
                 <ProductItem amount={1} />
             </div>
-            <div></div>
+            <div className={"cart-sheet-amount"}>
+                <div>סכום ביניים</div>
+                <div>{
+                    amounts ? amounts?.reduce((previous, current, index)=>{
+                        console.log("INDEX: "+index)
+                        return previous + current * (products && index < products.length ? products[index].price : 1)
+                    }, 0) : 0
+                }$</div>
+            </div>
+            <Link to={"/cart"} className={"cart-sheet-link"}>לצפייה בסל</Link>
         </div>
     )
 }
